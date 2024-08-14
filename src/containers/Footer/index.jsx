d5da2a04655b72ddc2c086398ea4a1bb5efe7530
saksharora <saksh.209302108@muj.manipal.edu>
2024-08-15 02:09:49 +0530

    Update index.jsx

--- src/containers/Footer/index.jsx
+++ src/containers/Footer/index.jsx
@@ -3,11 +3,15 @@ import { FOOTER_INFO } from "~/constants";
 const Footer = () => {
   const { content } = FOOTER_INFO;
   return (
-    <footer className="bg-primary py-12">
-      <div className="container mx-auto">
-        <p className="text-white text-center">{content}</p>
+    <footer className="bg-primary py-8 md:py-12">
+      <div className="container mx-auto px-4">
+        <p className="text-white text-center text-sm md:text-base lg:text-lg">
+          {content}
+        </p>
       </div>
     </footer>
   );
 };
+
 export default Footer;
+
